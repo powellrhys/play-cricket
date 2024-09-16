0547f3c29153dc63382ed84817b36b5dc59acac3
rhys powell <rhys.powell2@btinternet.com>
2024-09-16 17:02:17 +0100

    comments and readme update

--- functions.py
+++ functions.py
@@ -1,14 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
 
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd
 from datetime import datetime
+import pandas as pd
+import time
 
 
 def configure_driver():
@@ -17,6 +16,7 @@ def configure_driver():
     chrome_options = Options()
     chrome_options.add_argument("--log-level=3")
 
+    # Configure Driver with options
     service = Service(executable_path="chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.maximize_window()
@@ -26,28 +26,28 @@ def configure_driver():
 
 def login_to_play_cricket(driver, club, email, password):
 
+    # Open chrome on specific play cricket club
     driver.get(f"http://{club}.play-cricket.com/users/sign_in")
-
     time.sleep(2)
 
-    password_element = driver.find_element(By.ID, 'password')
-    password_element.send_keys(password)
+    # Enter email and password into login page
+    driver.find_element(By.ID, 'password').send_keys(password)
+    driver.find_element(By.ID, 'email').send_keys(email)
 
-    email_element = driver.find_element(By.ID, 'email')
-    email_element.send_keys(email)
-
+    # Click Login button
     i = 0
     while i < 5:
         try:
-            login_submit_button = driver \
-                .find_element(By.CLASS_NAME, "sc-bBHwJV")
-            login_submit_button.click()
             time.sleep(1)
+            driver.find_element(By.CLASS_NAME, "sc-bBHwJV") \
+                .click()
+
         except BaseException:
             pass
 
         i = i + 1
 
+    # Allow some time for to ensure the login was succesful
     time.sleep(3)
 
     return driver
@@ -55,12 +55,10 @@ def login_to_play_cricket(driver, club, email, password):
 
 def remove_cookies_pop_up(driver):
 
+    # Remove cookies pop up
     time.sleep(5)
-
-    cookie_popup = driver \
-        .find_element(By.CLASS_NAME, "onetrust-close-btn-handler")
-    cookie_popup.click()
-
+    driver.find_element(By.CLASS_NAME, "onetrust-close-btn-handler") \
+        .click()
     time.sleep(1)
 
     return driver
@@ -68,24 +66,20 @@ def remove_cookies_pop_up(driver):
 
 def query_batting_data(driver):
 
-    statistics_tab = driver.find_element(By.LINK_TEXT, "STATISTICS")
-    statistics_tab.click()
-
+    # Navigate to statistics tab
+    driver.find_element(By.LINK_TEXT, "STATISTICS").click()
     time.sleep(1)
 
-    filters_button = driver.find_element(By.CLASS_NAME, "btn-filter")
-    filters_button.click()
-
+    # Open data filter tab
+    driver.find_element(By.CLASS_NAME, "btn-filter").click()
     time.sleep(1)
 
-    min_innings_input = driver.find_element(By.ID, "atleast")
-    min_innings_input.send_keys(Keys.BACKSPACE + '1')
-
+    # Edit minimum filter to equal one
+    driver.find_element(By.ID, "atleast").send_keys(Keys.BACKSPACE + '1')
     time.sleep(1)
 
-    min_innings_input = driver.find_element(By.NAME, "commit")
-    min_innings_input.click()
-
+    # Update search paramaters
+    driver.find_element(By.NAME, "commit").click()
     time.sleep(1)
 
     return driver
@@ -93,57 +87,69 @@ def query_batting_data(driver):
 
 def collect_batting_data_summary_data(driver):
 
+    # Collect page source
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
+
+    # Parse the html to find the data
     table = soup.find('table')
 
+    # Collect table headers
     headers = []
     for th in table.find_all('th'):
         headers.append(th.text.strip())
 
+    # Create empty dataframe
     summary_df = pd.DataFrame(columns=headers)
 
+    # Define individual batting stats column headers
     indiviaul_batting_stats_columns = \
         ['SEASON', 'GAMES', 'INNS', 'NOT OUTS', 'RUNS', 'HIGH SCORE',
          'AVG', '50s', '100s', '4s', '6s', 'DUCKS', '%TEAM RUNS', 'PLAYER']
 
+    # Create empty individual batting stats dataframe
     batting_stats_df = \
         pd.DataFrame(columns=indiviaul_batting_stats_columns)
 
+    # Iterate through each page to collect batting stats for the year
     scan_pages = True
     while scan_pages:
         try:
             summary_df, page_df = collect_table_data(driver, headers,
                                                      summary_df)
-
             time.sleep(1)
 
+            # Iterate through each player and collect their batting data
             for player in page_df['PLAYER'].tolist():
-
+  
+                # Collect individual batting data
                 batting_stats_df = \
                     collect_individual_player_batting_data(driver, player,
                                                            batting_stats_df)
 
-            next_button = driver.find_element(By.LINK_TEXT, "Next")
-            next_button.click()
-
+            # Return to previous page
+            driver.find_element(By.LINK_TEXT, "Next").click()
             time.sleep(1)
 
         except BaseException:
+
+            # Exit while loop
             scan_pages = False
 
+    # Clean batting summary dataframe
     summary_df = summary_df.reset_index().drop(columns=['index'])
+
+    # Clean the individual batting stats dataframe
     batting_stats_df = batting_stats_df.reset_index() \
         .drop(columns=['index', 'SEASON', 'GAMES', 'INNS', 'NOT OUTS', 'RUNS',
                        'HIGH SCORE', 'AVG', '50s', '100s'])
 
+    # Join both dataframes on player name
     batting_df = summary_df.merge(batting_stats_df, left_on='PLAYER',
                                   right_on='PLAYER', how='inner')
 
+    # Write data to csv file
     batting_df.to_csv('batting_data.csv', index=False)
-
-    # summary_df.to_csv('batting.csv', index=False)
-    # batting_stats_df.to_csv('individual_batting.csv', index=False)
 
     return driver
 
@@ -151,29 +157,38 @@ def collect_batting_data_summary_data(driver):
 def collect_individual_player_batting_data(driver, player_name,
                                            batting_stats_df):
 
-    play_button = driver.find_element(By.LINK_TEXT, player_name)
-    play_button.click()
+    # Open indivial player batting stats
+    driver.find_element(By.LINK_TEXT, player_name).click()
 
+    # Load page source
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
+
+    # Locate player data
     table = soup.find('table')
 
+    # Iterate through each row in table and collect the data
     rows = []
     for tr in table.find_all('tr')[1:]:
         cells = tr.find_all('td')
         row = [cell.text.strip() for cell in cells]
         rows.append(row)
 
+    # Filter out incorrect data
     data = [row for row in rows if len(row) == len(batting_stats_df.columns)]
 
+    # Convert data into dataframe and append player name to dataframe
     page_df = pd.DataFrame(data, columns=batting_stats_df.columns)
     page_df['PLAYER'] = player_name
 
+    # Union existing data with new data
     batting_stats_df = pd.concat([batting_stats_df, page_df])
 
+    # Filter out old data - only keep data from the current season
     batting_stats_df = batting_stats_df[batting_stats_df['SEASON'] ==
                                         str(datetime.now().year)]
 
+    # Return to previous page
     driver.back()
 
     return batting_stats_df
@@ -181,20 +196,25 @@ def collect_individual_player_batting_data(driver, player_name,
 
 def collect_table_data(driver, columns, df):
 
+    # Collect page source code
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
     table = soup.find('table')
 
+    # Iterate through table to collect data
     rows = []
     for tr in table.find_all('tr')[1:]:
         cells = tr.find_all('td')
         row = [cell.text.strip() for cell in cells]
         rows.append(row)
 
+    # Remove non relevant data
     data = [row for row in rows if len(row) == len(columns)]
 
+    # Put data into a dataframe
     page_df = pd.DataFrame(data, columns=columns)
 
+    # Union new data with existing data
     df = pd.concat([df, page_df])
 
     return df, page_df
--- main.py
+++ main.py
@@ -28,15 +28,19 @@ password = os.getenv('password')
 driver = configure_driver()
 logger.info('Selenium Driver Configured')
 
+# Login to play cricket
 driver = login_to_play_cricket(driver, club, email, password)
 logger.info('Play Cricket Authentication Complete')
 
+# Disable cookies
 driver = remove_cookies_pop_up(driver)
 logger.info('Cookie Disabled')
 
+# Query batting data
 driver = query_batting_data(driver)
 logger.info('Batting Query Executed')
 
+# Collect batting data
 logger.info('Collecting Summary of Batting Data...')
 driver = collect_batting_data_summary_data(driver)
 logger.info('Summary of batting data collected')
