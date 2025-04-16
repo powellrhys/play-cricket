79644087adacf7fa2019bde0b3b0b3ed1d85ff13
rhys powell <rhys.powell2@btinternet.com>
2025-04-16 16:58:27 +0100

    how out batting page page added

--- frontend/pages/batting_player_analysis.py
+++ frontend/pages/batting_player_analysis.py
@@ -50,11 +50,11 @@ if st.experimental_user.is_logged_in:
     # Configure tab components
     tabs = st.tabs(['Runs Scored', 'How Out'])
 
-    # Render data 
+    # Render data
     with tabs[1]:
 
         # Render columns
-        cols = st.columns([1, 1, 1])
+        cols = st.columns([2, 1, 2, 2])
 
         # Render components within first column
         with cols[0]:
@@ -64,8 +64,15 @@ if st.experimental_user.is_logged_in:
                                   options=batters,
                                   key='selectbox-batter')
 
-        # Render components within 3rd column
+        # Render components in the 3rd columns
         with cols[2]:
+            plot_type = st.pills(label='Plot Type',
+                                 options=['Bar', 'Area', 'Line'],
+                                 default='Bar',
+                                 key='batting-plot-type')
+
+        # Render components within 4th column
+        with cols[3]:
 
             # Render season slider
             season = st.slider(label='Season Range',
@@ -88,4 +95,58 @@ if st.experimental_user.is_logged_in:
                 (how_out_df['SEASON'] <= season[1]) &
                 (how_out_df['PLAYER'] == bowler)]
 
-        st.dataframe(how_out_df)
+        # Melt the dataframe to long format
+        how_out_df = how_out_df \
+            .melt(id_vars=['SEASON', 'PLAYER'],
+                  value_vars=[col for col in how_out_df.columns if col not in ['SEASON', 'PLAYER']],
+                  var_name='Dismissal Type',
+                  value_name='Dismissal Count')
+
+        # Filter out NaN and data with zero records
+        how_out_df['Dismissal Count'] = how_out_df['Dismissal Count'].fillna(0)
+        how_out_df = how_out_df[how_out_df['Dismissal Count'] != 0.0]
+
+        # Define plot colour scheme
+        plot_colour_mapping = {
+            'BOWLED': '#316151',
+            'CAUGHT': '#FFE31A',
+            'LBW': '#A63D40',
+            'STUMPED': '#6495ED',
+            'RUN OUT': '#D2B48C',
+            'NOT OUT': '#E76F51',
+            'DID NOT BAT': '#2A9D8F',
+            'OTHER': '#E9C46A'
+        }
+
+        # Configure bar plot
+        if plot_type == 'Bar':
+            fig = px.bar(how_out_df,
+                         x='SEASON',
+                         y='Dismissal Count',
+                         color='Dismissal Type',
+                         barmode='group',
+                         title='Dismissals by Type per Season',
+                         color_discrete_map=plot_colour_mapping)
+
+        # Configure areas plot
+        if plot_type == 'Area':
+            fig = px.area(how_out_df,
+                          x='SEASON',
+                          y='Dismissal Count',
+                          color='Dismissal Type',
+                          markers=True,
+                          title='Dismissals by Type per Season (Line + Markers)',
+                          color_discrete_map=plot_colour_mapping)
+
+        # Configure line plot
+        if plot_type == 'Line':
+            fig = px.line(how_out_df,
+                          x='SEASON',
+                          y='Dismissal Count',
+                          color='Dismissal Type',
+                          markers=True,
+                          title='Dismissals by Type per Season (Line + Markers)',
+                          color_discrete_map=plot_colour_mapping)
+
+        # Render plot
+        st.plotly_chart(fig)
