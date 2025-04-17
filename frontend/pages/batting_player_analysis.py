7872c485b7e2ba75fcb591d0aa65d9c11aeecede
rhys powell <rhys.powell2@btinternet.com>
2025-04-17 16:51:47 +0100

    batting page added

--- frontend/pages/batting_player_analysis.py
+++ frontend/pages/batting_player_analysis.py
@@ -40,17 +40,95 @@ if st.experimental_user.is_logged_in:
     # Render page title
     st.title(f'{vars.club.capitalize()} CC Player Batting  Analysis')
 
-    # Read dismissal dataframe from blob
+    # Read batting dismissal dataframe from blob
     how_out_df = read_csv_from_blob(connection_string=vars.blob_connection_string,
                                     container_name='play-cricket',
                                     blob_name='batting_how_out.csv')
 
-    batters = how_out_df['PLAYER'].unique()
+    # Read batting data dataframe from blob
+    batting_df = read_csv_from_blob(connection_string=vars.blob_connection_string,
+                                    container_name='play-cricket',
+                                    blob_name='batting_data.csv')
+
+    batters_how_out = how_out_df['PLAYER'].unique()
+    batters = batting_df['PLAYER'].unique()
 
     # Configure tab components
     tabs = st.tabs(['Runs Scored', 'How Out'])
 
-    # Render data
+    # Render components in the first tab
+    with tabs[0]:
+
+        # Render columns
+        cols = st.columns([2, 2, 2, 2])
+
+        # Render components within first column
+        with cols[0]:
+
+            # Render selectbox for batters metric
+            batter = st.selectbox(label='Batter',
+                                  options=batters,
+                                  key='selectbox-batter-runs')
+
+        with cols[1]:
+            # Render selectbox for batting metric
+            batting_metric = st.selectbox(label='Metric',
+                                          options=['RUNS',
+                                                   'HIGH SCORE',
+                                                   '50s',
+                                                   '100s',
+                                                   '4s',
+                                                   '6s',
+                                                   'DUCKS'],
+                                          key='selectbox-batter-metric')
+
+        # Render components within 4th column
+        with cols[3]:
+
+            # Render season slider
+            season = st.slider(label='Season Range',
+                               min_value=datetime.now().year - 20,
+                               max_value=datetime.now().year,
+                               value=[datetime.now().year - 5, datetime.now().year],
+                               key='season-slider-runs')
+
+        # Render data source metadata badge
+        data_source_badge(blob_connection_string=vars.blob_connection_string,
+                          file_name='batting_data.csv')
+
+        # Transform season column to only include numeric values
+        batting_df = batting_df[batting_df['SEASON'] != 'ALL']
+        batting_df['SEASON'] = pd.to_numeric(batting_df['SEASON'], errors='coerce')
+        batting_df['HIGH SCORE'] = batting_df['HIGH SCORE'].str.replace('*', '', regex=False).astype('float')
+
+        # Filter data based on streamlit inputs
+        batting_df = \
+            batting_df[
+                (batting_df['SEASON'] >= season[0]) &
+                (batting_df['SEASON'] <= season[1]) &
+                (batting_df['PLAYER'] == batter)]
+
+        # Plot desired data in a bar chart
+        fig = px.bar(data_frame=batting_df,
+                     x='SEASON',
+                     y=batting_metric,
+                     title=f'{batting_metric.capitalize()} per Season - {batter}',
+                     labels={'RUNS': 'Runs', 'SEASON': 'Season'},
+                     text=batting_metric,
+                     color='INNS',
+                     color_continuous_scale='Greens')
+
+        # Update bar chart settings
+        fig.update_traces(textposition='outside')
+        fig.update_layout(xaxis=dict(type='category',
+                                     categoryorder='array',
+                                     categoryarray=sorted(batting_df['SEASON'])),
+                          uniformtext_minsize=8, uniformtext_mode='hide')
+
+        # Render bar plot
+        st.plotly_chart(fig)
+
+    # Render components in second tab
     with tabs[1]:
 
         # Render columns
@@ -61,7 +139,7 @@ if st.experimental_user.is_logged_in:
 
             # Render selectbox for bowlers metric
             bowler = st.selectbox(label='Batter',
-                                  options=batters,
+                                  options=batters_how_out,
                                   key='selectbox-batter')
 
         # Render components in the 3rd columns
@@ -78,7 +156,8 @@ if st.experimental_user.is_logged_in:
             season = st.slider(label='Season Range',
                                min_value=datetime.now().year - 20,
                                max_value=datetime.now().year,
-                               value=[datetime.now().year - 5, datetime.now().year])
+                               value=[datetime.now().year - 5, datetime.now().year],
+                               key='season-slider-how-out')
 
         # Render data source metadata badge
         data_source_badge(blob_connection_string=vars.blob_connection_string,
@@ -135,7 +214,7 @@ if st.experimental_user.is_logged_in:
                           y='Dismissal Count',
                           color='Dismissal Type',
                           markers=True,
-                          title='Dismissals by Type per Season (Line + Markers)',
+                          title='Dismissals by Type per Season',
                           color_discrete_map=plot_colour_mapping)
 
         # Configure line plot
@@ -145,8 +224,13 @@ if st.experimental_user.is_logged_in:
                           y='Dismissal Count',
                           color='Dismissal Type',
                           markers=True,
-                          title='Dismissals by Type per Season (Line + Markers)',
+                          title='Dismissals by Type per Season',
                           color_discrete_map=plot_colour_mapping)
+
+        fig.update_layout(xaxis=dict(type='category',
+                                     categoryorder='array',
+                                     categoryarray=sorted(how_out_df['SEASON'])),
+                          uniformtext_minsize=8, uniformtext_mode='hide')
 
         # Render plot
         st.plotly_chart(fig)
