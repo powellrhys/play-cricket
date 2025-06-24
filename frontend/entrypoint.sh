7e189e6d1d2e86c9f9012746b6cceb4ff571ad79
rhys powell <rhys.powell2@btinternet.com>
2025-06-24 21:23:43 +0100

    dockerfile refactor

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create the .streamlit directory if it doesn't exist
+mkdir -p /app/.streamlit
+
+# Generate secret.toml
+cat <<EOF > /app/.streamlit/secret.toml
+[auth]
+redirect_uri='${REDIRECT_URI}'
+cookie_secret='${COOKIE_SECRET}'
+
+[auth.auth0]
+domain='${AUTH0_DOMAIN}'
+client_id='${AUTH0_CLIENT_ID}'
+client_secret='${AUTH0_CLIENT_SECRET}'
+server_metadata_url='${AUTH0_METADATA_URL}'
+EOF
+
+# Start Streamlit
+exec "$@"
