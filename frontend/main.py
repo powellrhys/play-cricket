bfcb559f744426a13ac7618cf22313d5a5c4baf7
rhys powell <rhys.powell2@btinternet.com>
2025-06-24 21:40:34 +0100

    user returned to experimental user

--- frontend/main.py
+++ frontend/main.py
@@ -14,10 +14,10 @@ load_dotenv()
 vars = Variables()
 
 # Ensure user is authenticated to use application
-if not st.user.is_logged_in:
+if not st.experimental_user.is_logged_in:
     st.login('auth0')
 
 # Render application if user is logged in
-if st.user.is_logged_in:
+if st.experimental_user.is_logged_in:
     pg = get_navigation(club=vars.club)
     pg.run()
