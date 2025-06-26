c33d886765acbce307e1247e633504f61234fa00
rhys powell <rhys.powell2@btinternet.com>
2025-06-26 12:02:44 +0100

    change to quotes in entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-# Create the .streamlit directory if it doesn't exist
 mkdir -p /app/.streamlit
 
-# Generate secret.toml
 cat <<EOF > /app/.streamlit/secrets.toml
 [general]
-club=${CLUB}
-blob_connection_string=${BLOB_CONNECTION_STRING}
+club = "${CLUB:-}"
+blob_connection_string = "${BLOB_CONNECTION_STRING:-}"
 
 [auth]
-redirect_uri=${REDIRECT_URI}
-cookie_secret=${COOKIE_SECRET}
+redirect_uri = "${REDIRECT_URI:-}"
+cookie_secret = "${COOKIE_SECRET:-}"
 
 [auth.auth0]
-domain=${AUTH0_DOMAIN}
-client_id=${AUTH0_CLIENT_ID}
-client_secret=${AUTH0_CLIENT_SECRET}
-server_metadata_url=${AUTH0_SERVER_METADATA_URL}
+domain = "${AUTH0_DOMAIN:-}"
+client_id = "${AUTH0_CLIENT_ID:-}"
+client_secret = "${AUTH0_CLIENT_SECRET:-}"
+server_metadata_url = "${AUTH0_SERVER_METADATA_URL:-}"
 EOF
 
-exec streamlit run frontend/main.py --server.port $PORT
+exec streamlit run frontend/main.py --server.port "${PORT:-8501}"
 
 
 # #!/bin/bash
+
+# set -e
 
 # # Create the .streamlit directory if it doesn't exist
 # mkdir -p /app/.streamlit
@@ -33,19 +33,44 @@ exec streamlit run frontend/main.py --server.port $PORT
 # # Generate secret.toml
 # cat <<EOF > /app/.streamlit/secrets.toml
 # [general]
-# club='${CLUB}'
-# blob_connection_string='${BLOB_CONNECTION_STRING}'
+# club=${CLUB}
+# blob_connection_string=${BLOB_CONNECTION_STRING}
 
 # [auth]
-# redirect_uri='${REDIRECT_URI}'
-# cookie_secret='${COOKIE_SECRET}'
+# redirect_uri=${REDIRECT_URI}
+# cookie_secret=${COOKIE_SECRET}
 
 # [auth.auth0]
-# domain='${AUTH0_DOMAIN}'
-# client_id='${AUTH0_CLIENT_ID}'
-# client_secret='${AUTH0_CLIENT_SECRET}'
-# server_metadata_url='${AUTH0_METADATA_URL}'
+# domain=${AUTH0_DOMAIN}
+# client_id=${AUTH0_CLIENT_ID}
+# client_secret=${AUTH0_CLIENT_SECRET}
+# server_metadata_url=${AUTH0_SERVER_METADATA_URL}
 # EOF
 
-# # Start Streamlit
-# exec "$@"
+# exec streamlit run frontend/main.py --server.port $PORT
+
+
+# # #!/bin/bash
+
+# # # Create the .streamlit directory if it doesn't exist
+# # mkdir -p /app/.streamlit
+
+# # # Generate secret.toml
+# # cat <<EOF > /app/.streamlit/secrets.toml
+# # [general]
+# # club='${CLUB}'
+# # blob_connection_string='${BLOB_CONNECTION_STRING}'
+
+# # [auth]
+# # redirect_uri='${REDIRECT_URI}'
+# # cookie_secret='${COOKIE_SECRET}'
+
+# # [auth.auth0]
+# # domain='${AUTH0_DOMAIN}'
+# # client_id='${AUTH0_CLIENT_ID}'
+# # client_secret='${AUTH0_CLIENT_SECRET}'
+# # server_metadata_url='${AUTH0_METADATA_URL}'
+# # EOF
+
+# # # Start Streamlit
+# # exec "$@"
