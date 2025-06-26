d283b35c68e3af9788590334c33fb235a2edd74a
rhys powell <rhys.powell2@btinternet.com>
2025-06-26 14:22:11 +0100

    commented out code removed

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -e
 
 mkdir -p /app/.streamlit
@@ -21,56 +20,3 @@ server_metadata_url = "${AUTH0_SERVER_METADATA_URL:-}"
 EOF
 
 exec streamlit run frontend/main.py --server.port "${PORT:-8501}"
-
-
-# #!/bin/bash
-
-# set -e
-
-# # Create the .streamlit directory if it doesn't exist
-# mkdir -p /app/.streamlit
-
-# # Generate secret.toml
-# cat <<EOF > /app/.streamlit/secrets.toml
-# [general]
-# club=${CLUB}
-# blob_connection_string=${BLOB_CONNECTION_STRING}
-
-# [auth]
-# redirect_uri=${REDIRECT_URI}
-# cookie_secret=${COOKIE_SECRET}
-
-# [auth.auth0]
-# domain=${AUTH0_DOMAIN}
-# client_id=${AUTH0_CLIENT_ID}
-# client_secret=${AUTH0_CLIENT_SECRET}
-# server_metadata_url=${AUTH0_SERVER_METADATA_URL}
-# EOF
-
-# exec streamlit run frontend/main.py --server.port $PORT
-
-
-# # #!/bin/bash
-
-# # # Create the .streamlit directory if it doesn't exist
-# # mkdir -p /app/.streamlit
-
-# # # Generate secret.toml
-# # cat <<EOF > /app/.streamlit/secrets.toml
-# # [general]
-# # club='${CLUB}'
-# # blob_connection_string='${BLOB_CONNECTION_STRING}'
-
-# # [auth]
-# # redirect_uri='${REDIRECT_URI}'
-# # cookie_secret='${COOKIE_SECRET}'
-
-# # [auth.auth0]
-# # domain='${AUTH0_DOMAIN}'
-# # client_id='${AUTH0_CLIENT_ID}'
-# # client_secret='${AUTH0_CLIENT_SECRET}'
-# # server_metadata_url='${AUTH0_METADATA_URL}'
-# # EOF
-
-# # # Start Streamlit
-# # exec "$@"
--- frontend/main.py
+++ frontend/main.py
@@ -8,22 +8,6 @@ from functions.data_functions import (
 from functions.navigation import (
     get_navigation
 )
-# from functions.config import (
-#     generate_secrets_config_file
-# )
-# import time
-
-load_dotenv()
-
-# if 'secrets_initialized' not in st.session_state:
-#     generate_secrets_config_file()
-#     st.session_state['secrets_initialized'] = True
-#     progress_text = 'Configuring application backend'
-#     my_bar = st.progress(0, text=progress_text)
-#     for percent_complete in range(100):
-#         time.sleep(0.01)
-#         my_bar.progress(percent_complete + 1, text=progress_text)
-#     time.sleep(1)
 
 # Load environment variables
 load_dotenv()
