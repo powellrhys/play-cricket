fa09d7aa8d59f2b538d20425c1c56e0fd04ce5aa
rhys powell <rhys.powell2@btinternet.com>
2025-06-27 09:02:10 +0100

    testing of app setting injection

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 # Define azure app service module
 module "frontend" {
-    source = "git::https://github.com/powellrhys/powellrhys-iac.git//terraform/azure/app_service?ref=main"
+    source = "git::https://github.com/powellrhys/powellrhys-iac.git//terraform/azure/app_service?ref=feautre/secret_injection"
 
     app_service_name    = "play-cricket-streamlit-frontend"
     resource_group_name = var.app_service_resource_group
@@ -8,5 +8,5 @@ module "frontend" {
     service_plan_id     = data.azurerm_app_service_plan.app_service_plan.id
     docker_image        = var.docker_image_name
     docker_image_tag    = "latest"
-    app_settings        = {}
+    app_settings        = var.app_service_app_settings
 }
